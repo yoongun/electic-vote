8eee37b45fcb6546d307587db13084255e530799
cfr2ak <cfr2ak@gmail.com>
2020-02-22 11:50:05 +0900

    Change filename and add the test for the boolean logic

--- finite_field_test.go
+++ finite_field_test.go
@@ -1,4 +1,4 @@
-package galmug
+package lacan
 
 import "testing"
 
@@ -51,5 +51,30 @@ func TestFieldElemBoundary(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestFieldElemCompare(t *testing.T) {
+	cases := []struct {
+		desc 	string
+		elem1	FieldElem
+		elem2 	FieldElem
+		want	bool
+	}{
+		{"elem1 == elem2 (true case)", NewFieldElem(1, 7), NewFieldElem(1, 7), true},
+		{"elem1 == elem2 (false case)", NewFieldElem(2, 7), NewFieldElem(1, 7), false},
+		{"elem1 == elem2 (false case)", NewFieldElem(1, 7), NewFieldElem(1, 11), false},
+		{"elem1 != elem2 (true case)", NewFieldElem(1, 7), NewFieldElem(2, 7), true},
+		{"elem1 != elem2 (false case)", NewFieldElem(2, 13), NewFieldElem(2, 13), false},
+	}
+
+	for _, test := range cases {
+		t.Run(test.desc, func(t *testing.T) {
+			got1 := test.elem1.Eq(test.elem2)
+			got2 := test.elem1.Neq(test.elem2)
+
+			if got1 != test.want || got2 == test.want {
+				t.Errorf("Comparison does not match the value desired")
+			}
+		})
+	}
 }
